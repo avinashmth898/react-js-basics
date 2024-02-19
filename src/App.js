516de3be10e4c53044281c514be4f46add075109
“avinashmth898” <avinashmth898@gmail.com>
2024-02-19 21:50:31 +0530

    Component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
+import Chai from "./Chai";
 function App() {
   return (
     <div className="App">
+      <Chai />
       <h1>Learn React</h1>
     </div>
   );
---
+++ src/Chai.js
@@ -0,0 +1,7 @@
+function Chai() {
+    return(
+        <h1>High on Chai</h1>
+    )
+}
+
+export default Chai
